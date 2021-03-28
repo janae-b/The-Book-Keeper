974b5b4dae8e0b4602a79ff8c82192889bb71349
janae-b <janae.boersma@comcast.net>
2021-03-28 17:29:32 -0500

    fixed form

--- controllers/books.js
+++ controllers/books.js
@@ -22,12 +22,10 @@ function index(req, res) {
 }
 
 function create(req, res) {
-    for (let key in req.body) {
-      if (req.body[key] === '') delete req.body[key]
-    }
     const book = new Book(req.body)
     book.save(function(err) {
       if (err){ return res.redirect('/books/new')} 
-      res.redirect(`/books/${book._id}`)
+      console.log(book)
+      res.redirect(`/books/new`)
     })
   }
