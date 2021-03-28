3974942ffba0e69c632dd899ded51bb581ef1f85
janae-b <janae.boersma@comcast.net>
2021-03-28 10:46:54 -0500

    created book model

--- models/book.js
+++ models/book.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    review: String,
-
+    reviewer: String,
+    rating: {type: Number, min: 1, max: 5},
+    comment: String
+}, {
+timestamps:true
 })
 
 const bookSchema = new Schema({
     title: String,
-    
-})
+    author: String,
+    reviews: [reviewSchema]
+}, {timestamps:true})
+
+module.exports = mongoose.model("Book", bookSchema)
