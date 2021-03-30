2067c83dd5c1b0ca68abbc941b90705deabbe823
janae-b <janae.boersma@comcast.net>
2021-03-30 17:44:52 -0500

    updated nav bar

--- controllers/users.js
+++ controllers/users.js
@@ -10,8 +10,7 @@ module.exports = {
 
 function update(req, res) {
   User.findByIdAndUpdate(req.user._id, req.body, {new: true}) 
-  .then((user) => {
-    console.log(user)
+  .then(() => {
     res.redirect('/users/profile')
   })
 }
