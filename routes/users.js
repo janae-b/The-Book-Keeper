6c3d51dcd3ebe32e50da6b3678bd98f62089aa67
janae-b <janae.boersma@comcast.net>
2021-04-02 01:37:49 -0500

    cleaned css

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const usersCtrl = require("../controllers/users");
 
-
 router.get("/", isLoggedIn, usersCtrl.index);
 router.get("/profile", isLoggedIn, usersCtrl.showProfile)
 router.put("/profile", isLoggedIn, usersCtrl.update)
@@ -11,7 +10,5 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect("/auth/google");
 }
-
-
 
 module.exports = router;
